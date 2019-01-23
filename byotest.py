86b706e98dc0a40b26d37b1277fdf48ee6c582bd
Sarah Griggs <sgriggs177@yahoo.co.uk>
2019-01-23 20:57:27 +0000

    test_not_in function and test to fail this

--- byotest.py
+++ byotest.py
@@ -10,10 +10,14 @@ def test_not_equal(a, b):
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format (collection, item)
     
+def test_not_in(collection, item):
+    assert item not in collection, "{0} is contained in {1}".format (collection, item)
     
-#test_are_equal(number_of_evens([1,2,3,4,5]),2)
+    
+# test_are_equal(number_of_evens([1,2,3,4,5]),2)
 
 # test_not_equal(1, 1)
 
-test_is_in([2], 1)
+# test_is_in([2], 1)
 
+test_not_in([1], 1)

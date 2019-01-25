288bba2f32abed6ed8b135c30f508a4c811dae1b
Sarah Griggs <sgriggs177@yahoo.co.uk>
2019-01-25 21:30:10 +0000

    Function added to given multiple denominartions and raise an error if insuffient coins available

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,37 @@
 from byotest import *
 
-usd_coins = [100, 50, 25, 10, 5, 1]
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
+#dictionary of coins (20 of each denomination og coins)
+usd_coins = {
+    100:20,
+    50:20,
+    25:20,
+    10:20,
+    5:20,
+    1:20
+    }
+    
+eur_coins = {
+    100:20,
+    50:20,
+    20:20,
+    10:20,
+    5:20,
+    2:20,
+    1:20
+    }
 
 def get_change(amount, coins=eur_coins):
    
     change = []
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin)
+    for denomination in sorted(coins.keys(),reverse=True):
+        while denomination <= amount and coins[denomination] >0:
+            amount -= denomination
+            coins[denomination] -=1
+            change.append(denomination)
+    
+    #error message if insuffient coins to give coins        
+    if amount !=0:
+        raise Exception ("Sorry, there is insuffient coins to give change")
             
     return change
 
@@ -25,5 +47,6 @@ test_are_equal(get_change(3), [2, 1])
 test_are_equal(get_change(7), [5, 2])
 test_are_equal(get_change(9), [5, 2, 2])
 test_are_equal(get_change(35, usd_coins), [25, 10])
+test_are_equal(get_change(9, {5:1, 2:1, 1:2}), [5, 2, 1, 1])
 
 print("All tests pass!!")
